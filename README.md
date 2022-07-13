22f2d267d1b13a151f9a7621ac2c018215a277df
mafiono <nolimit.hackerr@gmail.com>
2022-07-14 06:51:55 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-qwkkm8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-qwkkm8)
---
+++ js/cookie.js
@@ -0,0 +1,166 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/assets/app/js/cookie.js":
+/*!*******************************************!*\
+  !*** ./resources/assets/app/js.js/cookie.js.js ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(_module, _exports) {
+
+setTimeout(function () {
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+
+    return "";
+  }
+
+  var cookieDesc = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie') || Object.getOwnPropertyDescriptor(HTMLDocument.prototype, 'cookie');
+
+  if (cookieDesc && cookieDesc.configurable) {
+    Object.defineProperty(document, 'cookie', {
+      get: function get() {
+        return cookieDesc.get.call(document);
+      },
+      set: function set(val) {
+        var accept = getCookie('acceptPolicy');
+        var name = val.match(/^[^=]+/);
+        var allowedNames = [];
+
+        if (accept == 'partly') {
+          allowedNames = ['XSRF-TOKEN', 'checkedProxy', 'lang_c', 'nuxgame _session', '__cf_bm', 'datr', '__gat_gtag_UA_170660750_1'];
+        }
+
+        name = name.length ? name[0] : '';
+
+        if (accept == '' || accept == 'full' || name == 'acceptPolicy' || allowedNames.includes(name)) {
+          // console.log('Set cookie ' + name);
+          cookieDesc.set.call(document, val);
+        } else {// console.log('Blocked cookie' + name);
+        }
+      }
+    });
+  }
+}, 2000); // setCookie('test1', 'qwerty', 30)
+// setCookie('test2', 'zxcvb', 30)
+
+/***/ }),
+
+/***/ 5:
+/*!*************************************************!*\
+  !*** multi ./resources/assets/app/js.js/cookie.js.js ***!
+  \*************************************************/
+/*! no static exports found */
+/***/ (function(module, _exports, __webpack_require__) {
+
+module.exports = __webpack_require__("Users/olgamakarenko/Sites/Nux-Website/resources/assets/app/js.js/cookie.js.js", "/resources/assets/app/js.js/cookie.js.js");
+
+
+/***/ })
+
+/******/ });
